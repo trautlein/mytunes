b3d4fe05f1cec8c8866b76c88ee6eb913b5587b8
trautlein <hans@trautlein.com>
2016-09-29 11:01:22 -0700

    Finished all of the SongModel tests
    Commented the SongModel.js file and have enabled all of the tests
    within SongSpec.js

--- client/models/SongModel.js
+++ client/models/SongModel.js
@@ -4,6 +4,21 @@ var SongModel = Backbone.Model.extend({
   play: function() {
     // Triggering an event here will also trigger the event on the collection
     this.trigger('play', this);
+  },
+
+  enqueue: function() {
+    // adds the next song to the bottom of the queue! how wonderful!
+    this.trigger('enqueue', this);
+  },
+
+  dequeue: function() {
+    // removes this item from the queue
+    this.trigger('dequeue', this);
+  },
+
+  ended: function() {
+    // this method triggers an event that tells you that you have ended
+    this.trigger('ended', this);
   }
 
 });
